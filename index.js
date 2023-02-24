62af5318c6b7ccaba77b0585ccc7c011de52fb68
bilaalgithub <bilaalkulane13@gmail.com>
2023-02-24 19:19:16 +0000

    added questions

--- index.js
+++ index.js
@@ -5,16 +5,67 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type:'input',
+        name:'email',
+        message:'Enter your email address',
+    },
+    {
+        type:'input',
+        name:'github',
+        message:'Enter your Github username',
+    },
+    {
+        type:'input',
+        name:'title',
+        message:'Whats the name of your project',
+    },
+    {
+        type:'input',
+        name:'description',
+        message:'Enter a short description of your project',
+    },
+    {
+        type:'list',
+        name:'license',
+        message:'which liscense would you like?',
+        choice: ["Apache 2.0", "MIT", "GNU 3.0", "None"]
+    },
+    {
+        type:'input',
+        name:'installation',
+        message:'which command should you use to run dependencies',
+        default:'npm i'
+    },
+    {
+        type:'input',
+        name:'testing',
+        message:'which command should you run to test',
+        default:'npm test'
+    },
+    {
+        type:'input',
+        name:'usage',
+        message:'what does the user need to know using the repo?'
+    },
+    {
+        type:'input',
+        name:'contribution',
+        message:'what does the user need to know about contributing to repo?'
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((response) => {
+        console.log(`Genrating README...`);
+        writeToFile('README.md', generateMarkdown({...response}))
+    })
 }
 
 // function call to initialize program
