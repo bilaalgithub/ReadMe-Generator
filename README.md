f5f88151c9e62e0b9133a3af4fc97b72fb23a457
bilaalgithub <bilaalkulane13@gmail.com>
2023-02-24 21:24:54 +0000

    added table contents

--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # ReadMe-Generator
   ![Github License](https://img.shields.io/badge/license-$(license)-blue.svg)
 
+  ## Table of Contents
+
+* [Installation](#installation)
+
+* [License](#license)
+
+* [Test](#test)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Questions](#questions)
+
+## Installation
+
+In-order to install all required dependencies, please run the following command:
+```
+npm i
+```
+
+
+## license
+```
+this project is licensed under APACHE 2.0 license.
+```
+
+## Test
+
+in-order to test please run the following commands:
+
+```
+npm test
+```
+
+## Usage
+```
+node v-16 or above
+```
+
+## Contributing
+```
+pull request
+```
+
+## Questions
+
+if you have any further questions please dont hesitate and contact me at bilaalkulane13@gmail.com
+
+otherwise please feel free to have a look at [bilaalGithub] (https://github.com/bilaalGithub).
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,66 @@
 // function to generate markdown for README
-function renderLicenseBadge(license) {
+const renderLicenseBadge = (license) => {
   if(license !== "None") {
     return `![Github License](https://img.shields.io/badge/license-$(license)-blue.svg)`
   }
   return ''
 }
+
+const renderLicenseLink = (license) => {
+  if(license !=="none") {
+    return `\n* [License](#license)\n`
+  }
+  return '';
+}
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
   ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+
+* [Installation](#installation)
+${renderLicenseLink(data.license)}
+* [Test](#test)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Questions](#questions)
+
+## Installation
+
+In-order to install all required dependencies, please run the following command:
+\`\`\`
+${data.installation}
+\`\`\`
+
+
+## license
+\`\`\`
+this project is licensed under ${data.license} license.
+\`\`\`
+
+## Test
+
+in-order to test please run the following commands:
+
+\`\`\`
+${data.test}
+\`\`\`
+
+## Usage
+\`\`\`
+${data.usage}
+\`\`\`
+
+## Contributing
+
+${data.contributing}
+
+## Questions
+
+if you have any further questions please dont hesitate and contact me at ${data.email}
+
+otherwise please feel free to have a look at [${data.github}] (https://github.com/${data.github}).
 
 `;
 }
